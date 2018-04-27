09c5fd55b40b4f6f6265e79e4525a6bc1d725521
Rebel1202 <36788257+Rebel1202@users.noreply.github.com>
2018-04-27 11:59:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
 console.log('Loaded!');
+function start_pos()
+{
+document.getElementById("https://scontent.famd1-2.fna.fbcdn.net/v/t1.0-1/16939647_113238395868605_5254059432279514871_n.jpg?_nc_cat=0&oh=5942f910ed52d6b97967de146e0bdb05&oe=5B6521C0").style.top = '550px';
+document.getElementById("https://scontent.famd1-2.fna.fbcdn.net/v/t1.0-1/16939647_113238395868605_5254059432279514871_n.jpg?_nc_cat=0&oh=5942f910ed52d6b97967de146e0bdb05&oe=5B6521C0").style.left = '550px'
+}
+
+function right() //Moves image to the right
+
+{
+document.getElementById("https://scontent.famd1-2.fna.fbcdn.net/v/t1.0-1/16939647_113238395868605_5254059432279514871_n.jpg?_nc_cat=0&oh=5942f910ed52d6b97967de146e0bdb05&oe=5B6521C0").style.top = '400px';
+document.getElementById("https://scontent.famd1-2.fna.fbcdn.net/v/t1.0-1/16939647_113238395868605_5254059432279514871_n.jpg?_nc_cat=0&oh=5942f910ed52d6b97967de146e0bdb05&oe=5B6521C0").style.left = '1000px';
+}
+
+function left() //moves image to the left
+
+{
+document.getElementById("https://scontent.famd1-2.fna.fbcdn.net/v/t1.0-1/16939647_113238395868605_5254059432279514871_n.jpg?_nc_cat=0&oh=5942f910ed52d6b97967de146e0bdb05&oe=5B6521C0").style.top = '0px';
+document.getElementById("https://scontent.famd1-2.fna.fbcdn.net/v/t1.0-1/16939647_113238395868605_5254059432279514871_n.jpg?_nc_cat=0&oh=5942f910ed52d6b97967de146e0bdb05&oe=5B6521C0").style.left = '0px';
+}
