57b0d4e7f034dbab07f8de6a81ffe2d909819056
Rebel1202 <36788257+Rebel1202@users.noreply.github.com>
2018-04-27 12:02:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+
 function start_pos()
 {
 document.getElementById("https://scontent.famd1-2.fna.fbcdn.net/v/t1.0-1/16939647_113238395868605_5254059432279514871_n.jpg?_nc_cat=0&oh=5942f910ed52d6b97967de146e0bdb05&oe=5B6521C0").style.top = '550px';
